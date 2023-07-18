b31b0e003f53484851f79e4624abdae46b734ce4
FE41PIW <eatdrink0507@gmail.com>
2023-07-19 01:10:31 +0900

    Feat : 추천검색어 보여주기

--- src/AutoComplete/index.tsx
+++ src/AutoComplete/index.tsx
@@ -1,7 +1,38 @@
-export const AutoComplete = ({ searchValue }: Props) => {
-  return <div>{searchValue}</div>;
+import style from "./AutoComplete.module.css";
+import { useState, useEffect } from "react";
+
+export const AutoComplete = ({ recommend, searchValue }: Props) => {
+  const [list, setList] = useState<any>();
+
+  useEffect(() => {
+    const r = recommend?.data?.filter(
+      (e: Object) =>
+        e.sickCd.includes(searchValue) || e.sickNm.includes(searchValue)
+    );
+    setList(r);
+  }, [searchValue]);
+
+  return (
+    <div className={style.Div}>
+      <div className={style.InnerDiv}>
+        {!list?.length && "검색어 없음"}
+        {list?.map((e: Object, i: number) => (
+          <div key={i}>
+            <span className={style.code}>{e.sickCd}</span> {e.sickNm}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 type Props = {
+  recommend: { data: [Object] };
+
   searchValue: string;
+};
+
+type Object = {
+  sickCd: string;
+  sickNm: string;
 };
