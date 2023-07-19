12be3fa88c9bdd81429be7264635a0d7675b753b
FE41PIW <eatdrink0507@gmail.com>
2023-07-19 22:13:08 +0900

    Feat : CacheAPI return값 추가

--- src/CacheAPI.tsx
+++ src/CacheAPI.tsx
@@ -7,15 +7,17 @@ export class CacheAPI {
   }
 
   static createCache(str: string) {
-    axios
+    return axios
       .get(`${import.meta.env.VITE_APP_API}/sick`, {
         params: { q: str },
       })
       .then(async (res) => {
         console.info("calling api");
+
         if (res.data.length) {
           (await caches.open(str)).put(str, new Response(JSON.stringify(res)));
-        } else return "no result";
+          return res;
+        }
       });
   }
 }
